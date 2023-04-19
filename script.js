7cf314a2a0e3b2bc6f79599f78284aded76aecde
Micael Macedo <micaelm2009@hotmail.com>
2023-04-18 21:30:18 -0300

    feat: add fontAwesome Icons and nav-toggle script

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".card");
+const cardbuttons = document.querySelectorAll(".cardButton")
+const navToggles = document.querySelectorAll(".nav-toggle")
+const navButtons = document.querySelectorAll(".nav-button")
 function showCard (button) {
     const cardSelected = $(button).attr("name")
+
+    cardbuttons.forEach(cardbutton => {
+            $(cardbutton).removeClass("selected");
+    });
     cards.forEach(card => {
         if($(card).attr("id") === cardSelected){
             $(card).addClass("selected");
+            $(button).addClass("selected");
         }else{
             $(card).removeClass("selected");
         }
     });
+}
+
+function displayMenu(menuSelected) {
+    let div = $(menuSelected).parent()
+    let menu = $(div).find(".nav-toggle")
+    if($(menuSelected).hasClass("selected")){
+        $(menuSelected).removeClass("selected")
+        navToggles.forEach(navToggle => {
+            $(navToggle).removeClass("selected")
+        });
+    }else{
+        navButtons.forEach(navButton => {
+            $(navButton).removeClass("selected")
+        });
+        navToggles.forEach(navToggle => {
+            $(navToggle).removeClass("selected")
+        });
+    
+        $(menu).toggleClass("selected")
+        $(menuSelected).toggleClass("selected")
+    }
+
 }
